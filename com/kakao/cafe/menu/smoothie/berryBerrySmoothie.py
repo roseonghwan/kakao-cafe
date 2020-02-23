29937f2a660953a4cf3c7f957ffa27a2baa81cfc
joshua-dev <msh01175@gmail.com>
2020-02-24 04:31:41 +0900

    Test Success: Higher order inheritance

---
+++ com/kakao/cafe/menu/smoothie/berryBerrySmoothie.py
@@ -0,0 +1,49 @@
+from menu.smoothie.smoothie import Smoothie
+
+
+class BerryBerrySmoothie(Smoothie):
+    def __init__(self):
+        super().__init__()
+        self.__mixedBerry = 1
+        self.name = 'BerryBerrySmoothie'
+        self.__price = 5000
+        self.__groundIce = 400
+
+    def getName(self) -> str:
+        return self.name
+
+    def setName(self, name: str) -> None:
+        self.name = name
+        return
+
+    def getPrice(self) -> int:
+        return self.__price
+
+    def setPrice(self, price: int) -> None:
+        self.__price = price
+        return
+
+    def isIced(self) -> bool:
+        return True
+
+    def setIced(self) -> None:
+        return
+
+    def getGroundIce(self) -> int:
+        return self.__groundIce
+
+    def setGroundIce(self, groundIce: int) -> None:
+        self.__groundIce = groundIce
+        return
+
+    def getMixedBerry(self) -> int:
+        return self.__mixedBerry
+
+    def setMixedBerry(self, mixedBerry: int) -> None:
+        self.__mixedBerry = mixedBerry
+        return
+
+    def addBerry(self, amount: int) -> None:
+        self.setMixedBerry(self.getMixedBerry() + amount)
+        self.setPrice(self.getPrice() + amount * 500)
+        return
---
+++ com/kakao/cafe/testutil/testSmoothie.py
@@ -0,0 +1,30 @@
+from menu.smoothie.smoothie import Smoothie
+from menu.smoothie.berryBerrySmoothie import BerryBerrySmoothie
+import unittest
+
+
+class TestSmoothie(unittest.TestCase):
+    def setUp(self):
+        self.berryBerrySmoothie = BerryBerrySmoothie()
+        self.berryBerrySmoothie.addBerry(3)
+        try:
+            self.impossible = Smoothie()
+
+        except TypeError as TE:
+            self.impossible = TE.__str__()
+
+    def testInstantiation(self):
+        self.assertEqual(
+            self.impossible,
+            "Can't instantiate abstract class Smoothie with abstract methods getGroundIce, getName, getPrice, isIced, setGroundIce, setIced, setName, setPrice"
+        )
+
+    def testBerryBerrySmoothie(self):
+        self.assertEqual(self.berryBerrySmoothie.getName(),
+                         'BerryBerrySmoothie')
+        self.assertEqual(self.berryBerrySmoothie.getPrice(), 6500)
+        self.assertEqual(self.berryBerrySmoothie.isIced(), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
