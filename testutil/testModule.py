f7aa0a8837b0a1edc2cee729997c4b8469ceb557
Seonghwan <nsh1998@kookmin.ac.kr>
2020-03-20 23:45:41 +0900

    PaymentManager Update

--- testutil/testModule.py
+++ testutil/testModule.py
@@ -53,17 +53,41 @@ class TestModule(unittest.TestCase):
         except TypeError as TE:
             self.impossible = TE.__str__()
 
-    def testPaymentSystem_cardPay(self):  # 카드결제 테스트
-        self.paymentManager.selectPaymentWay()
-        self.assertEqual(self.paymentManager.getPaymentSystem(), "카드")
+    def testCardPay(self):  # 카드결제 테스트
+        self.paymentManager.setCardBalance(10000)  # 초기 카드잔액 설정
+        self.paymentManager.setPoint(100)  # 초기 포인트 설정
+        current_point = self.paymentManager.getPoint()  # 초기 포인트의 값을 저장
+        self.orderTaker.getAddlist().append(
+            'Americano')  # 예시로 Americano 1개의 가격을 가져오기 위한 설정
+        self.orderTaker.getAddlist().append(1)
+        self.orderTaker.addAllPrice(1)
+        self.paymentManager.setTtotalPrice(3000)  # 위의 Americano 1개 가격으로 설정
+        current_price = self.paymentManager.getTotalPrice(
+        )  # 포인트를 사용하기 전 원래의 총 가격
+        self.paymentManager.selectPaymentWay()  # 결제 시작
+        self.assertEqual(self.paymentManager.getPaymentSystem(),
+                         "카드")  # 카드로 결제한게 맞는지 확인
+        self.assertEqual(self.paymentManager.getPoint(),
+                         current_point + self.orderTaker.getAllPrice() *
+                         0.03)  # 포인트를 사용하지 않고!! 적립하여 포인트 테스트
+        self.assertEqual(self.paymentManager.getPoint(),
+                         0)  # 포인트를 사용하여!! 포인트가 0이 되는 것을 테스트
+        self.assertEqual(self.paymentManager.getTotalPrice(), current_price -
+                         current_point)  # 포인트를 사용하여!! 할인된 총 가격을 테스트
 
     def testPaymentSystem_cashPay(self):  # 현금결제 테스트
-        self.paymentManager.selectPaymentWay()
-        self.assertEqual(self.paymentManager.getPaymentSystem(), "현금")
-
-    def testPaymentSystem_gifticonPay(self):  # 기프티콘 결제 테스트
-        self.paymentManager.selectPaymentWay()
-        self.assertEqual(self.paymentManager.getPaymentSystem(), "기프티콘")
+        self.paymentManager.setCash(10000)
+        current_cash = self.paymentManager.getCash()
+        self.orderTaker.getAddlist().append(
+            'Americano')  # 예시로 Americano 1개의 가격을 가져오기 위한 설정
+        self.orderTaker.getAddlist().append(1)
+        self.orderTaker.addAllPrice(1)
+        self.paymentManager.setTtotalPrice(3000)
+        self.paymentManager.selectPaymentWay()  # 결제 시작
+        self.assertEqual(self.paymentManager.getPaymentSystem(),
+                         "현금")  # 현금으로 결제한게 맞는지 확인
+        self.assertEqual(self.paymentManager.getChange(), current_cash -
+                         self.orderTaker.getAllPrice())  # 거스름돈이 맞는지 테스트
 
 
 if __name__ == '__main__':
